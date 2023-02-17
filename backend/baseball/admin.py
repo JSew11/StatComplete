4d2df5aead10f1397eb2dc3b611f648b1aef6005
Joshua Seward <joshuaseward11@gmail.com>
2023-02-17 16:59:21 +0000

    made migrations

--- backend/baseball/admin.py
+++ backend/baseball/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .models.coach import Coach
 from .models.coach_stats import CoachStats
 from .models.player import Player
+from .models.player_stats import PlayerStats
 
 # Register your models here.
 admin.site.register(Coach)
 admin.site.register(CoachStats)
 admin.site.register(Player)
+admin.site.register(PlayerStats)
