73c9398729601ec2952b194fd9cb05ea7a20fb19
Joshua Seward <joshuaseward11@gmail.com>
2023-02-16 21:32:18 -0800

    Fixed the base settings so hopefully the git workflow will work now

--- backend/statcomplete_api/settings/base.py
+++ backend/statcomplete_api/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='very-bad-prod-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -79,8 +78,8 @@ WSGI_APPLICATION = 'statcomplete_api.wsgi.application'
 
 DB_USERNAME=os.environ.get("DB_USERNAME")
 DB_PASSWORD=os.environ.get("DB_PASSWORD")
-DB_HOST='db'
-DB_PORT=5432
+DB_HOST=os.environ.get('DB_HOST')
+DB_PORT=os.environ.get('DB_PORT')
 DB_DATABASE=os.environ.get("DB_DATABASE")
 DB_IS_AVAIL = all([
         DB_USERNAME, 
