17800869d4cd01bdd53c78cdcbefeb9a257ed5d5
Joshua Seward <joshuaseward11@gmail.com>
2023-02-27 02:31:37 +0000

    Installed and set up drf knox authentication

--- backend/statcomplete_api/settings/base.py
+++ backend/statcomplete_api/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -144,6 +145,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
