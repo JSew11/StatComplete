6e7bd47c6782dd2584ea43a348970414056bc049
Joshua Seward <joshuaseward11@gmail.com>
2023-02-28 04:25:00 +0000

    refactored create user to be register user

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from knox import views as knox_views
 
-from core.views.create_user_view import CreateUserView
+from core.views.register_user_view import RegisterUserView
 from core.views.login_view import LoginView
 from core.views.manage_user_view import ManageUserView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('create/', CreateUserView.as_view(), name='create'),
+    path('register/', RegisterUserView.as_view(), name='create'),
     path('profile/', ManageUserView.as_view(), name='profile'),
     path('login/', LoginView.as_view(), name='knox_login'),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
--- backend/core/views/register_user_view.py
+++ backend/core/views/register_user_view.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 
 from ..serializers.user_serializer import UserSerializer
 
-class CreateUserView (generics.CreateAPIView):
+class RegisterUserView (generics.CreateAPIView):
     """Create user API view.
     """
     serializer_class = UserSerializer
