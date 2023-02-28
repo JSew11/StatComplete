fe2fc40aa44275f1d4168ac65acb7d8f4ef4722b
Joshua Seward <joshuaseward11@gmail.com>
2023-02-28 04:33:01 +0000

    Updated user serializer to show more fields

--- backend/core/serializers/user_serializer.py
+++ backend/core/serializers/user_serializer.py
@@ -7,7 +7,7 @@ class UserSerializer (serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
     
     def create(self, validated_data):
