cb2bb906b36a32c30a8ff6e2e14592f9fd33011e
Joshua Seward <joshuaseward11@gmail.com>
2023-03-02 23:49:19 +0000

    Created team model

--- backend/baseball/admin.py
+++ backend/baseball/admin.py
@@ -4,9 +4,15 @@ from .models.coach import Coach
 from .models.coach_competition_stats import CoachCompetitionStats
 from .models.player import Player
 from .models.player_competition_stats import PlayerCompetitionStats
+from .models.competition import Competition
+from .models.organization import Organization
+from .models.team import Team
 
 # Register your models here.
 admin.site.register(Coach)
 admin.site.register(CoachCompetitionStats)
 admin.site.register(Player)
 admin.site.register(PlayerCompetitionStats)
+admin.site.register(Competition)
+admin.site.register(Organization)
+admin.site.register(Team)
