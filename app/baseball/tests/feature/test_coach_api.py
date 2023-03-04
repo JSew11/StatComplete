9be39ff7d770b9ec76e9537abb5e6f8069ec6474
Joshua Seward <joshuaseward11@gmail.com>
2023-03-04 20:13:03 +0000

    updated coach fixtures and implemented them in the feature tests

--- app/baseball/tests/feature/test_coach_api.py
+++ app/baseball/tests/feature/test_coach_api.py
@@ -7,16 +7,12 @@ from baseball.models.coach import Coach
 class TestCoachDetailsApi (APITestCase):
     """Tests for endpoints defined in CoachDetails view.
     """
-    fixtures = ['user']
+    fixtures = ['user', 'coach']
 
     def setUp(self) -> None:
         """Set up necessary objects for testing and log in the appropriate test user(s).
         """
-        self.test_coach: Coach = Coach.objects.create(
-            first_name = 'Test',
-            last_name = 'Coach',
-        )
-        self.test_coach_id = self.test_coach.id
+        self.test_coach: Coach = Coach.objects.get(first_name='Test', last_name='Coach')
         self.client = APIClient()
         user = User.objects.get(username='DeveloperAdmin')
         self.client.force_authenticate(user)
@@ -25,7 +21,7 @@ class TestCoachDetailsApi (APITestCase):
     def test_coach_by_id(self):
         """Test the GET endpoint for getting a coach by its associated uuid.
         """
-        response = self.client.get(path=f'/api/baseball/coaches/{self.test_coach_id}/')
+        response = self.client.get(path=f'/api/baseball/coaches/{self.test_coach.id}/')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(self.test_coach.first_name, response.data.get('first_name'))
         self.assertEqual(self.test_coach.last_name, response.data.get('last_name'))
@@ -36,35 +32,29 @@ class TestCoachDetailsApi (APITestCase):
         updated_coach_field = {
             'birth_date':'2000-01-01',
         }
-        response = self.client.put(path=f'/api/baseball/coaches/{self.test_coach_id}/', data=updated_coach_field, format='json')
+        response = self.client.put(path=f'/api/baseball/coaches/{self.test_coach.id}/', data=updated_coach_field, format='json')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(updated_coach_field.get('birth_date'), response.data.get('birth_date'))
     
     def test_delete_coach(self):
         """Test the DELETE endpoint for deleting a coach using its associated uuid.
         """
-        delete_response = self.client.delete(path=f'/api/baseball/coaches/{self.test_coach_id}/')
+        delete_response = self.client.delete(path=f'/api/baseball/coaches/{self.test_coach.id}/')
         self.assertEqual(status.HTTP_204_NO_CONTENT, delete_response.status_code)
 
-        get_response = self.client.get(path=f'/api/baseball/coaches/{self.test_coach_id}/')
+        get_response = self.client.get(path=f'/api/baseball/coaches/{self.test_coach.id}/')
         self.assertEqual(status.HTTP_404_NOT_FOUND, get_response.status_code)
 
 class TestCoachListApi (APITestCase):
     """Tests for endpoints defined in CoachList view.
     """
-    fixtures = ['user']
+    fixtures = ['user', 'coach']
 
     def setUp(self):
         """Set up necessary objects for testing.
         """
-        Coach.objects.create(
-            first_name = 'Test',
-            last_name = 'Coach',
-        )
-        Coach.objects.create(
-            first_name = 'Another',
-            last_name = 'Coach',
-        )
+        Coach.objects.get(first_name='Test', last_name='Coach')
+        Coach.objects.get(first_name='Another', last_name='Coach')
         self.client = APIClient()
         user = User.objects.get(username='DeveloperAdmin')
         self.client.force_authenticate(user)
