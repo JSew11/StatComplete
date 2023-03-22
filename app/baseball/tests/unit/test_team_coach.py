6dc461b814769aa060b30c814964f047879ae4ee
Joshua Seward <joshuaseward11@gmail.com>
2023-03-22 00:08:53 +0000

    Wrote test class for the team player model

--- app/baseball/tests/unit/test_team_coach.py
+++ app/baseball/tests/unit/test_team_coach.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 
 from baseball.models.team_coach import TeamCoach
 
-class TestCoachModel (TestCase):
-    """Tests for the coach model.
+class TestTeamCoachModel (TestCase):
+    """Tests for the team coach model.
     """
     fixtures = ['organization', 'competition', 'team', 'competition_team', 'coach', 'team_coach']
 
@@ -15,7 +15,7 @@ class TestCoachModel (TestCase):
         return super().setUp()
     
     def test_team_coach_string(self):
-        """Test the overwritten __str__ method for the coach model.
+        """Test the overwritten __str__ method for the team coach model.
         """
-        coach_string_representation = str(self.test_team_coach.coach)+' #1'
+        coach_string_representation = str(self.test_team_coach.coach)+f' #{self.test_team_coach.jersey_number}'
         self.assertEqual(coach_string_representation, str(self.test_team_coach))
