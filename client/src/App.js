064bc9c6ab0618700a599c8003de3881de73e3e8
Joshua Seward <joshuaseward11@gmail.com>
2023-03-25 18:53:54 +0000

    Made basic nav bar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import Header from './core/Header';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Header />
+        <Footer />
       </Fragment>
     );
   }
