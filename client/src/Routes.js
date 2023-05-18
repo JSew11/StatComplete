6a4a8454039e24a8a8fefd85e551111bbe72fa28
Joshua Seward <joshuaseward11@gmail.com>
2023-05-18 23:02:34 +0000

    added todo

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -8,11 +8,12 @@ import Register from './pages/register';
 
 const Routes = () => (
   <Switch>
+    <Route exact path='login/' Component={Login} />
+    <Route exact path='register/' Component={Register} />
     <Route Component={Layout}>
       <Route path='/' Component={Dashboard} />
+      {/* TODO: make a 404 page here */}
     </Route>
-    <Route path='login/' Component={Login} />
-    <Route path='register/' Component={Register} />
   </Switch>
 );
 
