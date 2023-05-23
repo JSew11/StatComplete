866f5ef7b2ea76855bfc947f7e5ec013bb17a4af
Joshua Seward <joshuaseward11@gmail.com>
2023-05-23 16:25:04 +0000

    moved auth pages directory into user pages directory

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,8 +3,8 @@ import { Route, Routes as Switch } from 'react-router-dom';
 
 import Layout from 'src/pages/layout';
 import Dashboard from 'src/pages/dashboard';
-import Login from 'src/pages/auth/login';
-import Register from 'src/pages/auth/register';
+import Login from 'src/pages/user/auth/login';
+import Register from 'src/pages/user/auth/register';
 import UserProfile from 'src/pages/user/profile';
 
 const Routes = () => (
