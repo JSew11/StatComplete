9fbba22344a6ecb76268dbb5bf5aa502a99a96ed
Joshua Seward <joshuaseward11@gmail.com>
2023-05-25 15:55:27 +0000

    added an example of how I want to get dashboard data

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+
 const Dashboard = () => {
+  const [ tempVar, setTempVar ] = useState('');
+
+  const { isLoggedIn } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      // TODO: call API to get user-specific dashboard data
+      setTempVar('Logged In');
+    } else {
+      // TODO: call API to get generic dashboard data
+      setTempVar('Not Logged In');
+    }
+  }, []);
+
   return (
-    <h1>Home</h1>
+    <Container fluid>
+      <Row className='p-2'>
+        <Col className='text-center'><h1>{tempVar}</h1></Col>
+      </Row>
+    </Container>
   );
 }
 
