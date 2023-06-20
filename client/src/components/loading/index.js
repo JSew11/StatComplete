745fcd1b481319a170a5294cd7874462bde8d19c
Joshua Seward <joshuaseward11@gmail.com>
2023-06-20 15:26:50 +0000

    Updated loading component to only use mui

--- client/src/components/loading/index.js
+++ client/src/components/loading/index.js
@@ -1,20 +1,15 @@
-import {
-  Row,
-  Col,
-  Spinner,
-} from 'reactstrap';
-
-import './index.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
 
 export default function Loading() {
   return (
     <>
-      <Row className='p-2 align-items-center'>
-        <Col className='text-center'><Spinner className='loading-spinner' /></Col>
-      </Row>
-      <Row className='p-2 align-items-center'>
-        <Col className='text-center'><h4 className='loading-text'>Loading</h4></Col>
-      </Row>
+      <Grid item className='p-2 align-items-center'>
+        <div className='text-center'><CircularProgress /></div>
+      </Grid>
+      <Grid item className='p-2 align-items-center'>
+        <div className='text-center'><h4 className='loading-text'>Loading</h4></div>
+      </Grid>
     </>
   );
 }
